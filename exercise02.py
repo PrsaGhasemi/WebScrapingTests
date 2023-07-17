cf379b69e0264d0609b245ea0dd4e6d528b2a0e8
Parsa Ghasemi <parsa.ghasemi2012@gmail.com>
2023-07-17 18:30:20 +0330

    question 2 completed

--- exercise02.py
+++ exercise02.py
@@ -1,6 +1,24 @@
 enteredText = input("enter your text here: ")
 
+numberValue = []
+stringValue = []
+unknownValue = []
+
+
 def typeSeperator(text):
     seperatedValues = text.split()
     for value in seperatedValues:
-        if type(value) == str
+        if value.isalpha():
+            stringValue.append(value)
+        elif value.isnumeric():
+            numberValue.append(value)
+        else:
+            unknownValue.append(value)
+
+    print(f"words: {stringValue}")
+    print(f"numbers: {numberValue}")
+    if len(unknownValue) != 0:
+        print(f"and it also includes {unknownValue} as another value type")
+
+
+typeSeperator(enteredText)
